0c5f244cf5cd7706ae49d7d0f412f452d294abc6
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-09-21 13:46:44 -0700

    ADD Functio

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const getMob = document.getElementById("father");
 const phones = [
     {
         brand: 'Samsung',
-        img:"https://m.media-amazon.com/images/I/41KRhotx+PL.jpg",
+        img: "https://m.media-amazon.com/images/I/41KRhotx+PL.jpg",
         model: 'S20',
         ram: 8,
         rom: 256,
         camera: '20 megapixel',
-        price:159000,
+        price: 159000,
     },
     {
         brand: 'Xiomi',
-        img:"https://images.priceoye.pk/xiaomi-redmi-note-10-pakistan-priceoye-124uk.jpg",
+        img: "https://images.priceoye.pk/xiaomi-redmi-note-10-pakistan-priceoye-124uk.jpg",
         model: 'note10',
         ram: 4,
         rom: 64,
@@ -22,7 +22,7 @@ const phones = [
     },
     {
         brand: 'Infinix',
-        img:"https://rakistan.pk/wp-content/uploads/2021/11/infinix-zero-x-pro-pakistan-priceoye-im0ai-500x500-1.webp",
+        img: "https://rakistan.pk/wp-content/uploads/2021/11/infinix-zero-x-pro-pakistan-priceoye-im0ai-500x500-1.webp",
         model: 'z10',
         ram: 2,
         rom: 16,
@@ -31,16 +31,16 @@ const phones = [
     },
     {
         brand: 'Tecno',
-        img:"https://images.priceoye.pk/tecno-spark-10-pakistan-priceoye-2144l.jpg",
+        img: "https://images.priceoye.pk/tecno-spark-10-pakistan-priceoye-2144l.jpg",
         model: 'spark10',
         ram: 12,
         rom: 512,
         camera: '25 megapixel',
-        price: 39999 ,
+        price: 39999,
     },
     {
         brand: 'Iphone',
-        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkb7zxjf33D_bOzSqpJdYZtFRS9_d9w2VVHOcAn7HTDtOyONYMoH5Iz1uQQeh2qm17DyA&usqp=CAU",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkb7zxjf33D_bOzSqpJdYZtFRS9_d9w2VVHOcAn7HTDtOyONYMoH5Iz1uQQeh2qm17DyA&usqp=CAU",
         model: '14',
         ram: 4,
         rom: 1024,
@@ -49,7 +49,7 @@ const phones = [
     },
     {
         brand: 'Oppo',
-        img:"https://cf.shopee.com.my/file/cfb2a159ee8d06bc3d0a0046ec34d060",
+        img: "https://cf.shopee.com.my/file/cfb2a159ee8d06bc3d0a0046ec34d060",
         model: 'F11',
         ram: 8,
         rom: 256,
@@ -58,21 +58,21 @@ const phones = [
     },
     {
         brand: 'Vivo',
-        img:"https://newglacier.pk/wp-content/uploads/2022/09/y20.jpg",
+        img: "https://newglacier.pk/wp-content/uploads/2022/09/y20.jpg",
         model: 'y20',
         ram: 4,
         rom: 64,
         camera: '8 megapixel',
-        price:26999 ,
+        price: 26999,
     },
     {
         brand: 'Huawei',
-        img:"https://mobiles.dailypakistan.com.pk/wp-content/uploads/2020/08/HUAWEIP3-600x600.jpg",
+        img: "https://mobiles.dailypakistan.com.pk/wp-content/uploads/2020/08/HUAWEIP3-600x600.jpg",
         model: 'P30 Pro',
         ram: 8,
         rom: 512,
         camera: '40 megapixel',
-        price:26999 ,
+        price: 26999,
     },
     // {
     //     brand: 'Itel',
@@ -87,26 +87,26 @@ const phones = [
 ]
 
 const footer = [
-   {
-    img:'https://static.priceoye.pk/images/percentage.svg',
-    text1:'100% Original Products',
-   },
-   {
-    img:'https://static.priceoye.pk/images/percentage.svg',
-    text1:'Fast delivery',
-   },
-   {
-    img:'https://static.priceoye.pk/images/percentage.svg',
-    text1:'Fast delivery',
-   }
+    {
+        img: 'https://static.priceoye.pk/images/percentage.svg',
+        text1: '100% Original Products',
+    },
+    {
+        img: 'https://static.priceoye.pk/images/percentage.svg',
+        text1: 'Fast delivery',
+    },
+    {
+        img: 'https://static.priceoye.pk/images/percentage.svg',
+        text1: 'Fast delivery',
+    }
 ]
 
 
 
 //  console.log(phones[1].brand);
 
-for(let i = 0; i<phones.length;i++){
-    getMob.innerHTML+=`<div class="main-mob"><h3>${phones[i].brand}</h3>
+for (let i = 0; i < phones.length; i++) {
+    getMob.innerHTML += `<div class="main-mob"><h3>${phones[i].brand}</h3>
     <img src="${phones[i].img}" class="image">
     <h4>Model:${phones[i].model}</h4>
     <h4>Ram:${phones[i].ram} Gb</h4>
@@ -116,30 +116,43 @@ for(let i = 0; i<phones.length;i++){
     <button onclick="addtoCart(${i})" class="cart">Add To Cart</button>
     </div>`
 }
-const cartArry = []; 
-function addtoCart(index){
+const cartArry = [];
+
+function addtoCart(index) {
     // console.log(phones[index]);
-    if(cartArry.includes(phones[index])){
+    if (cartArry.includes(phones[index])) {
 
         for (let i = 0; i < cartArry.length; i++) {
-            if (cartArry[i]===phones[index]) {
-                cartArry[i].Quantity+=1
+            if (cartArry[i] === phones[index]) {
+                cartArry[i].Quantity += 1
             }
         }
 
-    }else{
-        phones[index].Quantity=1
+    } else {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Item Added To Cart',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        phones[index].Quantity = 1
         cartArry.push(phones[index])
+
     }
     console.log(cartArry);
 }
-function gotoCart(){
-    window.location="./cart.html";
 
+
+
+
+function gotoCart() {
+    const cartItems = JSON.stringify(cartArry);
+    localStorage.setItem('cartArry', cartItems)
+    window.location = "./cart.html";
 }
 
-let string = JSON.stringify(cartArry)
-localStorage.setItem("cartArry", string)
+
 
 
 
--- cart.js
+++ cart.js
@@ -1,6 +1,78 @@
 const cartAdditem = document.getElementById('father');
+const totalCartPrice = document.getElementById('totalBill');
 
-function gotohome(){
-    window.location="./index.html";
+
+
+let retString = localStorage.getItem("cartArry")
+let retArray = JSON.parse(retString)
+// console.log(retArray);
+// cartAdditem.innerHTML += ""
+// for (let i = 0; i < retArray.length; i++) {
+//     // console.log(retArray[i]);
+
+//     cartAdditem.innerHTML += `<div class="main-mob"><h3>${retArray[i].brand}</h3>
+//     <img src="${retArray[i].img}" class="image">
+//     <h4>Model:${retArray[i].model}</h4>
+//     <h4>Ram:${retArray[i].ram} Gb</h4>
+//     <h4>Rom:${retArray[i].rom}Gb</h4>
+//     <h4>Camra:${retArray[i].camera}</h4>
+//     <h4>Price: ${retArray[i].price}</h4>
+//     <h4>Quantity: ${retArray[i].Quantity}</h4>
+//     <h4>TotlePrice: ${retArray[i].price * retArray[i].Quantity}</h4>
+//     <button onclick="delte(${i})" class="cart">Delete</button>
+//     <button onclick="add(${i})" class="less">+</button>
+//     <button onclick="sub(${i})" class="less">-</button>
+//     </div>`
+
+// }
+render()
+
+function render() {
+    cartAdditem.innerHTML = ""
+    for (let i = 0; i < retArray.length; i++) {
+
+        retArray[i].TotalPrice = + `${retArray[i].price * retArray[i].Quantity}`
+        // console.log(retArray[i]);
+
+        cartAdditem.innerHTML += `<div class="main-mob"><h3>${retArray[i].brand}</h3>
+        <img src="${retArray[i].img}" class="image">
+        <h4>Model:${retArray[i].model}</h4>
+        <h4>Ram:${retArray[i].ram} Gb</h4>
+        <h4>Rom:${retArray[i].rom}Gb</h4>
+        <h4>Camra:${retArray[i].camera}</h4>
+        <h4>Price: ${retArray[i].price}</h4>
+        <h4>Quantity: ${retArray[i].Quantity}</h4>
+        <h4>TotlePrice: ${retArray[i].TotalPrice}</h4>
+        <button onclick="delte(${i})" class="cart">Delete</button>
+        <button onclick="add(${i})" class="less">+</button>
+        <span>${retArray[i].Quantity}</span>
+        <button onclick="sub(${i})" class="less">-</button>
+        </div>
+        `
+    }
+}
+
+function add(index) {
+    retArray[index].Quantity += 1
+    render()
+}
+function sub(index) {
+    retArray[index].Quantity -= 1
+    render()
+}
+
+
+
+
+function delte(index) {
+
+    retArray.splice(index, 1);
+    render()
+}
+
+
+function gotohome() {
+    window.location = "./index.html";
 
 }
+
