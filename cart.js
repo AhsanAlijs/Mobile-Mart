0a34686e1572b397b0b2113d91df9215f8dbfec4
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-09-27 10:29:32 -0700

    total cart price in if condition

--- cart.js
+++ cart.js
@@ -29,11 +29,11 @@ render()
 
 function render() {
     cartAdditem.innerHTML = ""
+    let full = 0 ;
     for (let i = 0; i < retArray.length; i++) {
-
+        totalCartPrice.innerHTML=''
         retArray[i].TotalPrice = + `${retArray[i].price * retArray[i].Quantity}`
         // console.log(retArray[i]);
-
         cartAdditem.innerHTML += `<div class="main-mob"><h3>${retArray[i].brand}</h3>
         <img src="${retArray[i].img}" class="image">
         <h4>Model:${retArray[i].model}</h4>
@@ -47,19 +47,29 @@ function render() {
         <button onclick="add(${i})" class="less">+</button>
         <span>${retArray[i].Quantity}</span>
         <button onclick="sub(${i})" class="less">-</button>
-        </div>
-        `
-    }
-}
+        </div>`
+        
 
+const totalItems =  retArray[i].TotalPrice
+full += totalItems
+        // totalCartPrice.innerHTML+=`${retArray[i].TotalPrice}`
+    }
+    
+    if(full===0){
+        totalCartPrice.innerHTML=`<p class="last">All Over Cart Price: ${full} PKR</p>`
+        }else{
+        totalCartPrice.innerHTML+=`<p class="last">All Over Cart Price: ${full} PKR</p>`
+    }
+    
+}
 function add(index) {
     retArray[index].Quantity += 1
     render()
 }
 function sub(index) {
     retArray[index].Quantity -= 1
-    if(retArray[index].Quantity===0){
-        retArray.splice(index,1)
+    if (retArray[index].Quantity === 0) {
+        retArray.splice(index, 1)
     }
     render()
 }
@@ -68,16 +78,24 @@ function sub(index) {
 
 
 function delte(index) {
-
     retArray.splice(index, 1);
-
-    
     render()
 }
-
 
 function gotohome() {
     window.location = "./index.html";
 
 }
+
+// function total() {
+//     totalCartPrice.innerHTML = ""
+//     let cartTOtal = 0;
+//     for(i = 0; i < retArray.length; i++){
+//        console.log(retArray[i].TotalPrice);
+//         //console.log(itemtotal);
+        
+//     }
+//     // totalCartPrice.innerHTML= `<p>total price: ${cartTOtal}</p>`
+// }
+//     total()
 
